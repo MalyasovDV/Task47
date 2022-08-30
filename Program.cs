a6a672c0c05824efb9518f2ecadf89972a3a7a07
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-30 16:31:08 +0800

    Task Done

--- Program.cs
+++ Program.cs
@@ -5,19 +5,19 @@
     return (float)Math.Round((Decimal)(mantissa * rnd.Next(-10,10)), 1);
 }
 
-void ArrayGeneration(int m, int n, ref float[,] array)
+void ArrayGeneration(ref float[,] array)
 {
-    for (int i = 0; i < m; ++i)
-        for (int j = 0; j < n; ++j)
+    for (int i = 0; i < array.GetLength(0); ++i)
+        for (int j = 0; j < array.GetLength(1); ++j)
             array[i,j] = NextFloat();
 }
 
 void Output(int m, int n, ref float[,] array)
 {   
     Console.WriteLine("\n Here's the array");
-    for (int i = 0; i < m; ++i)
+    for (int i = 0; i < array.GetLength(0); ++i)
     {
-        for (int j = 0; j < n; ++j)
+        for (int j = 0; j < array.GetLength(1); ++j)
             Console.Write(+ array[i,j] + ";  ");
         Console.WriteLine();
     }
@@ -29,5 +29,5 @@ m = int.Parse(Console.ReadLine());
 n = int.Parse(Console.ReadLine());
 float[,] array = new float[m,n];
 
-ArrayGeneration(m, n, ref array);
+ArrayGeneration(ref array);
 Output(m, n, ref array);
