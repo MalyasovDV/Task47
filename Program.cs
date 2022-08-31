ac8a16fdbc568f83889167525e2f591fe71619b7
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-31 14:01:06 +0800

    Task done2

--- Program.cs
+++ Program.cs
@@ -2,32 +2,32 @@
 {
     Random rnd = new Random();
     double mantissa = (rnd.NextDouble() * 2.0) - 1.0;
-    return (float)Math.Round((Decimal)(mantissa * rnd.Next(-10,10)), 1);
+    return (float)Math.Round((mantissa * rnd.Next(-10,10)), 1);
 }
 
-void ArrayGeneration(ref float[,] array)
+void MatrixGeneration(ref float[,] matrix)
 {
-    for (int i = 0; i < array.GetLength(0); ++i)
-        for (int j = 0; j < array.GetLength(1); ++j)
-            array[i,j] = NextFloat();
+    for (int i = 0; i < matrix.GetLength(0); ++i)
+        for (int j = 0; j < matrix.GetLength(1); ++j)
+            matrix[i,j] = NextFloat();
 }
 
-void Output(int m, int n, ref float[,] array)
+void Output(int m, int n, ref float[,] matrix)
 {   
-    Console.WriteLine("\n Here's the array");
-    for (int i = 0; i < array.GetLength(0); ++i)
+    Console.WriteLine("\n Here's the matrix");
+    for (int i = 0; i < matrix.GetLength(0); ++i)
     {
-        for (int j = 0; j < array.GetLength(1); ++j)
-            Console.Write(+ array[i,j] + ";  ");
+        for (int j = 0; j < matrix.GetLength(1); ++j)
+            Console.Write(+ matrix[i,j] + ";  ");
         Console.WriteLine();
     }
 }
 
 int m,n;
-Console.WriteLine("Enter scales of the array M x N");
+Console.WriteLine("Enter scales of the matrix M x N");
 m = int.Parse(Console.ReadLine());
 n = int.Parse(Console.ReadLine());
-float[,] array = new float[m,n];
+float[,] matrix = new float[m,n];
 
-ArrayGeneration(ref array);
-Output(m, n, ref array);
+MatrixGeneration(ref matrix);
+Output(m, n, ref matrix);
